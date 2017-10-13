c05beea2a3f29546c712e1be38c6295fd55239c5
IKGapirov <IKGapirov@gmail.com>
2017-10-13 20:31:33 +0500

    build.sh: Update

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 mkdir output
 
-make -C $(pwd) O=output msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output unofficial_kmini3g_defconfig
 make -j2 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
