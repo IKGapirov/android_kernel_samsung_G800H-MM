09cbf952d4cc22e0724064cc92bdb8aac2f9385a
IKGapirov <IKGapirov@gmail.com>
2017-11-27 10:21:32 +0500

    defconfig: Add CONFIG_IP_NF_TARGET_TTL

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,9 +2,11 @@
 
 export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
-mkdir output
+#mkdir output
 
 make -C $(pwd) O=output unofficial_kmini3g_defconfig
+export KBUILD_BUILD_USER=Gapirov
+export KBUILD_BUILD_HOST=Build
 make -j2 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
